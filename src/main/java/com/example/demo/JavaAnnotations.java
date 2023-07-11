11edc356b6597cd925675820ec3a6897ad687676
ooleon <ooleon@hotmail.com>
2023-07-11 18:54:37 -0300

    Update

---
+++ src/main/java/com/example/demo/JavaAnnotations.java
@@ -0,0 +1,110 @@
+package com.example.demo;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+	String userRole() default "GUEST";
+	
+//	~~Complete the interface~~
+	int budgetLimit() default 100;
+}
+
+class FamilyMember {
+
+//	~~Complete this line~~
+@FamilyBudget(userRole = "senior", budgetLimit = 500 )
+	public void seniorMember(int budget, int moneySpend) {
+		System.out.println("Senior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+
+//	~~Complete this line~~
+@FamilyBudget(userRole = "junior", budgetLimit = 200 )
+	public void juniorUser(int budget, int moneySpend) {
+		System.out.println("Junior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+}
+
+public class JavaAnnotations {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String role = in.next();
+			int spend = in.nextInt();
+			try {
+				Class annotatedClass = FamilyMember.class;
+				
+				Method[] methods = annotatedClass.getMethods();
+				
+				for (Method method : methods) {
+					if (method.isAnnotationPresent(FamilyBudget.class)) {
+						
+						FamilyBudget family = method
+								.getAnnotation(FamilyBudget.class);
+						
+						String userRole = family.userRole();
+
+//						int budgetLimit = ~~Complete this line~~;
+						int budgetLimit = family.budgetLimit();
+						
+						if (userRole.equals(role)) {
+//							if(~~Complete this line~~){
+							if(spend<=budgetLimit){
+								method.invoke(FamilyMember.class.newInstance(),
+										budgetLimit, spend);
+							}else{
+								System.out.println("Budget Limit Over");
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			testCases--;
+		}
+	}
+}
+
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String role = in.next();
+            int spend = in.nextInt();
+            try {
+                Class annotatedClass = FamilyMember.class;
+                Method[] methods = annotatedClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(FamilyBudget.class)) {
+                        FamilyBudget family = method
+                                .getAnnotation(FamilyBudget.class);
+                        String userRole = family.userRole();
+                        int budgetLimit = family.budgetLimit();
+                        if (userRole.equals(role)) {
+                            if(spend<=budgetLimit){
+                                method.invoke(FamilyMember.class.newInstance(),
+                                        budgetLimit, spend);
+                            }else{
+                                System.out.println("Budget Limit Over");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            testCases--;
+        }
+    }
+}
