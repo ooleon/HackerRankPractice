f5a04132a2c474429942f3b569205592b75634b8
ooleon <ooleon@hotmail.com>
2023-09-30 23:48:07 -0300

    update

--- src/main/java/com/example/done/VistorPattern/JavaVistorPattern.java
+++ src/main/java/com/example/done/VistorPattern/JavaVistorPattern.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.done.VistorPattern;
 
 import java.io.*;
@@ -12,8 +8,9 @@ import java.util.regex.*;
 
 /**
  *
- * @author leo
- *         https://www.hackerrank.com/challenges/java-vistor-pattern/problem
+ *	@author leo
+ *	hackerrank DONE
+ *	https://www.hackerrank.com/challenges/java-vistor-pattern/problem
  */
 
 enum Color {
